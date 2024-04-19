34257f91b489e39d2183cb657b06a4cef60d63cb
Errol-Hassall <Errol.hassall99@gmail.com>
2024-04-19 10:50:09 +0000

    update profile endpoint

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,5 +1,7 @@
 import {Hono} from "hono";
 import {PrismaClient} from "@prisma/client";
+import {validator} from "hono/validator";
+import {z} from "zod";
 
 const prisma = new PrismaClient();
 
@@ -15,6 +17,35 @@ profile.get("/", async (c) => {
 	}
 
 	return c.json({ user: { email: user.email, name: user.name, id: user.id } });
+});
+
+const updateSchema = z.object({
+	name: z.string().min(1).max(100),
+});
+
+profile.patch("/",
+	validator("json", (value, c) => {
+		const parsed = updateSchema.safeParse(value);
+		if (!parsed.success) {
+			return c.text("Invalid!", 401);
+		}
+		return parsed.data;
+	}),
+	async (c) => {
+	const userId = c.get("jwtPayload").id;
+	const { name } = c.req.valid("json");
+
+	const user = await prisma.user.findUnique({ where: { id: userId } });
+
+	if (!user) {
+		return c.text("User doesn't exist", 401);
+	}
+
+	console.log({name: c.body});
+
+	await prisma.user.update({ where: { id: userId }, data: { name } });
+
+	return c.json({ user: { email: user.email, name, id: user.id } });
 });
 
 export default profile;
